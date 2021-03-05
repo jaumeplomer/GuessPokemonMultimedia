0a4b7416bce62dd499b91a9a9e30bd2ce23461d1
Jaume Plomer Suau <jaumeplomer@paucasesnovescifp.cat>
2021-03-06 00:36:07 +0100

    Intent per anar a LlistaPokemonActivity desde MainActivity

--- app/src/main/java/com/example/guesspokemon/MainActivity.java
+++ app/src/main/java/com/example/guesspokemon/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.guesspokemon;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.guesspokemon.Pokemon.LlistaPokemonActivity;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               Intent i = new Intent(view.getContext(), LlistaPokemonActivity.class);
+               startActivity(i);
             }
         });
     }
